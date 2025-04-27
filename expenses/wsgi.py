0a8c6e7c5d3f581b217443971282ce06e6819c6c
Bileks <bilisakumera@gmail.com>
2025-04-28 01:44:59 +0300

    wsgi

--- expenses/wsgi.py
+++ expenses/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the default settings module for the 'django' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expense_tracker_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # Create an application object that can be used by WSGI servers (like Gunicorn).
 application = get_wsgi_application()
